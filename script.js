c83d4ecd69c516fe30ffe439241027eeaee33dfe
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-04-20 20:49:01 -0400

    fix bug

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const bookmarksContainer = document.getElementById("bookmarks-container");
 
 let bookmarks = [];
 
-// Show Modal, focus on Input
+// Show Modal, Focus on Input
 function showModal() {
   modal.classList.add("show-modal");
   websiteNameEl.focus();
@@ -26,25 +26,25 @@ window.addEventListener("click", (e) =>
 // Validate Form
 function validate(nameValue, urlValue) {
   const expression =
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
   const regex = new RegExp(expression);
   if (!nameValue || !urlValue) {
-    alert("Please submit values for both fields");
+    alert("Please submit values for both fields.");
     return false;
   }
   if (!urlValue.match(regex)) {
-    alert("Please provide a valid web address");
+    alert("Please provide a valid web address.");
     return false;
   }
   // Valid
   return true;
 }
 
-// Build Bookmarks Dom
+// Build Bookmarks
 function buildBookmarks() {
   // Remove all bookmark elements
   bookmarksContainer.textContent = "";
-  // Build Items
+  // Build items
   bookmarks.forEach((bookmark) => {
     const { name, url } = bookmark;
     // Item
@@ -77,7 +77,7 @@ function buildBookmarks() {
   });
 }
 
-// Fetch Bookmarks
+// Fetch bookmarks
 function fetchBookmarks() {
   // Get bookmarks from localStorage if available
   if (localStorage.getItem("bookmarks")) {
@@ -97,32 +97,36 @@ function fetchBookmarks() {
 
 // Delete Bookmark
 function deleteBookmark(url) {
+  // Loop through the bookmarks array
   bookmarks.forEach((bookmark, i) => {
     if (bookmark.url === url) {
       bookmarks.splice(i, 1);
     }
   });
-  //   Update bookmarks array in loacalStorage, re-populate DOM
+  // Update bookmarks array in localStorage, re-populate DOM
   localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   fetchBookmarks();
 }
 
-// Handle Form Data
 function storeBookmark(e) {
   e.preventDefault();
   const nameValue = websiteNameEl.value;
   let urlValue = websiteUrlEl.value;
-  if (!urlValue.includes("http://", "https://")) {
+  // Add 'https://' if not there
+  if (!urlValue.includes("https://") && !urlValue.includes("http://")) {
     urlValue = `https://${urlValue}`;
   }
+  // Validate
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  // Set bookmark object, add to array
   const bookmark = {
     name: nameValue,
     url: urlValue,
   };
   bookmarks.push(bookmark);
+  // Set bookmarks in localStorage, fetch, reset input fields
   localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   fetchBookmarks();
   bookmarkForm.reset();
@@ -132,5 +136,5 @@ function storeBookmark(e) {
 // Event Listener
 bookmarkForm.addEventListener("submit", storeBookmark);
 
-// On load, Fetch Bookmarks
+// On Load, Fetch Bookmarks
 fetchBookmarks();
